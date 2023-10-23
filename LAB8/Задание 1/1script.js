d7093ff14f43ae3720b52184edb6c619a7a7d9c5
Aulia <94062075+MazanovaYulia@users.noreply.github.com>
2023-10-23 17:27:13 +0700

    Add files via upload

---
+++ LAB8/Задание 1/1script.js
@@ -0,0 +1,8 @@
+//1 задание - Калькулятор любви
+
+rand = Math. round(Math.random()*100);
+
+let person1 = prompt('Твое имя:');
+let person2 = prompt('Имя партнера:');
+
+alert(`Совместимость имен ${person1} и ${person2} равна ${rand} процентам!`);
